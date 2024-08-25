7358b6cb8062eb3b91606e8870cb71c704a6295d
Hrithik06 <el99el03@gmail.com>
2024-08-25 22:18:38 +0530

    Removed Images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import TestCard from './components/Cards/TestCard';
 import {
     Header,
     Container,
--- src/components/Cards/BrandCard.jsx
+++ src/components/Cards/BrandCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import michelinLogo from '../../assets/michelin-logo-1900x450.png';
-import pirelliLogo from '../../assets/pirelli-logo-2100x550.png';
+
 const BrandCard = ({ brand }) => {
     return (
         <div className="xl:w-56 w-52 md:w-32 md:h-full flex flex-col items-center justify-around border border-gray-300 shadow-md rounded-lg gap-1 py-1 text-gray-500">
